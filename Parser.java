13d81f8c89f18c892f657e4841d9c790eee1268f
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-05-17 22:09:44 +0200

    Parser Update

--- Parser.java
+++ Parser.java
@@ -1,27 +1,60 @@
 import java.util.ArrayList;
+//package com.mkyong.xml.dom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+
+
 
 public class Parser {
     ArrayList<token> tList;
     String TK;  //holds the token
-    Parser(ArrayList<token> e)
-    {
+    DocumentBuilderFactory dFactory;
+    DocumentBuilder dBuilder;
+    Document doc;
+    ArrayList<Element> elements=new ArrayList<>();
+    String xml;   //The xml buffer
+
+    Parser(ArrayList<token> e) throws Exception
+    {  
+        dFactory=DocumentBuilderFactory.newInstance();
+        dBuilder=dFactory.newDocumentBuilder();
+        doc = dBuilder.newDocument(); 
         for(token i: e)
         {
             tList.add(i);
         }
     }
 
-    public void Parse()
-    {
-        SPLProgr();
-    }
-
     public void SPLProgr()
     {
+        xml+="<SPLProgr>";
         if(!tList.isEmpty())
         {
-            
+            if(tList.get(0).contents=="proc")
+            {
+                ProcDefs();
+            }
+            else if(tList.get(0).contents=="main")
+            {
+                
+            }
         }
+        xml+="</SPLProgr";
     }
 
     public void ProcDefs()
